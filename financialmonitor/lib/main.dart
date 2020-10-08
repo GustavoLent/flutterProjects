774adf70ef0813670479f1d005e931ffcd255cb1
Gustavo Lima Lent <gustavo.lent@unesp.br>
2020-10-08 16:33:28 -0300

    Removendo baner de debug.

--- financialmonitor/lib/main.dart
+++ financialmonitor/lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'MoFi',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
